02ff5abe9b3dc2f2ad5f8a36a787b031d2ac9616
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2024-05-07 15:54:34 +0200

    Update Kotlin to 2.0.0-RC2 and migrate to the new compose compiler gradle plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,7 @@ pluginManagement {
 
     plugins {
         kotlin("multiplatform").version(extra["kotlin.version"] as String)
+        kotlin("plugin.compose").version(extra["kotlin.version"] as String)
         id("org.jetbrains.compose").version(extra["compose.version"] as String)
     }
 }
